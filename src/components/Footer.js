82fc4fc5462ff58205ba3f0da8496cf61b36c544
bigmosi <kinyeramo@gmail.com>
2022-07-27 18:12:09 +0300

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <h3 className="footer-h3">
+      <span>Created By</span>
+      <a className="footer-link" href="https://github.com/bigmosi"> Kinyera Amos</a>
+    </h3>
+  </footer>
+);
+
+export default Footer;
